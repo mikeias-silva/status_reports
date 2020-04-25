c4d79f34cabc1d8894cb5a4c42a5c80a0d537db4
Mikeias Silva <mikeias07@hotmail.com>
2020-04-25 13:07:17 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <div>
+          <thead>
+            <tr>
+
+              
+              <td>
+                1
+              </td>
+              <td>
+                2
+              </td>
+            </tr>
+          </thead>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
